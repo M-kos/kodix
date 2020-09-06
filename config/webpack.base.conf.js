2fd1303bf3ee69265630adafef71efba11cd8b75
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-09-06 11:27:46 +0300

    init

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,89 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@': PATHS.src
+    }
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, url: false }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: '' } }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`
+    }),
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html'
+    }),
+    new CopyWebpackPlugin([{ from: `${PATHS.src}/assets`, to: PATHS.assets }])
+  ]
+}
