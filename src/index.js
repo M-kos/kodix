504979cbf78289afc7e770064e52d7ee25dfc7f6
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-09-08 00:00:58 +0300

    App

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+import { HttpRequest } from './js/services/http'
+import { TableRow } from './js/components/TableRow'
+import { formHandler } from './js/services/form'
+import { url } from './js/constants/url'
+
 import './styles/styles.css'
+
+const ready = async () => {
+  let state = []
+  const table = document.getElementById('table-body')
+  const form = document.getElementById('add-block-form')
+  const fillDiv = document.createElement('div')
+
+  const render = () => {
+    table.innerHTML = ''
+
+    state.forEach((elem) => {
+      new TableRow({
+        data: elem,
+        parrent: table,
+        onDelete,
+      }).render()
+    })
+  }
+
+  const onDelete = ({ id }) => {
+    state = state.filter((el) => el.id !== id)
+    render()
+  }
+
+  const createHandler = (data) => {
+    state.push(data)
+    render()
+  }
+
+  fillDiv.classList.add('fill-row')
+  fillDiv.innerText = 'Loading...'
+  table.appendChild(fillDiv)
+
+  try {
+    state = await HttpRequest.getData(url)
+
+    if (state) {
+      fillDiv.remove()
+      render()
+    }
+  } catch (error) {
+    console.error(error)
+    fillDiv.innerText = 'Error'
+  }
+
+  formHandler(form, createHandler)
+}
+
+document.addEventListener('DOMContentLoaded', ready)
